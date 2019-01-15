50002702ae7a3ebaf8dadd2def0c038a69a80a6a
Hen Chow <henchow@gmail.com>
2019-01-16 07:03:12 +1100

    update to include button type

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -10,26 +10,68 @@ class Button extends React.Component {
     this.onClick = this.onClick.bind(this);
   }
 
+  /**
+   * @description handler for when user clicks on the component
+   */
   onClick() {
     if (this.props.onClick) {
-      this.onClick(this.props);
+      this.props.onClick(this.props);
     }
   }
 
   render() {
     const classes = classNames('button', this.props.className, `button--${this.props.color}`);
 
+    if (this.props.buttonType === 'submit') {
+      return (
+        <button
+          type="submit"
+          id={this.props.id}
+          className={classes}
+          onClick={this.onClick}
+        >
+          {this.props.children}
+        </button>
+      )
+    }
+
     return (
-      <button id={this.props.id} className={classes} onClick={this.onClick}>{this.props.children}</button>
+      <button
+        type="button"
+        id={this.props.id}
+        className={classes}
+        onClick={this.onClick}
+      >
+        {this.props.children}
+      </button>
     )
   }
 }
 
 Button.propTypes = {
+  /**
+   * unique Id for the component
+   */
   id: PropTypes.string,
+
+  /**
+   * css classes applied on the component
+   */
   className: PropTypes.string,
+
+  /**
+   * color of the component
+   */
   color: PropTypes.oneOf(['grey', 'light-blue']),
+
+  /**
+   * any children rendered in the component
+   */
   children: PropTypes.string,
+
+  /**
+   * the type of button
+   */
   buttonType: PropTypes.oneOf(['submit', 'button']),
 };
 
